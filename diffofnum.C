f21a790e15683d60917bc052a3c93ae8d47b817a
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-03-09 16:15:52 +0530

    diffofnum.C
    difference of numbers

---
+++ diffofnum.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+	int x1,y1,z1,x2,y2,z2;
+	scanf("\n %d%d",&x1,&y1);
+	scanf("\n %d%d",&x2,&y2);
+	z1=y1-x1;
+	z2=y2-x2;
+	printf("%d",z1);
+	printf("\n%d",z2);
+}
+
