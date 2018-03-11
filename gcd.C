4c6933417f90e62370338419be2b6c5dd41757fc
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-03-11 20:48:41 +0530

    gcd.C
    gcd

--- gcd.C
+++ gcd.C
@@ -5,7 +5,7 @@ void main()
 	scanf("%d %d",&n1,&n2);
 	for(i=1;i<=n1&&i<=n2;i++)
 	{
-	  if(n1%i==0 && n2%i==0)        //checks if i is factor of n1 and n2
+	  if(n1%i==0 && n2%i==0)        
 	  {
 	      gcd=i;
 	  }
